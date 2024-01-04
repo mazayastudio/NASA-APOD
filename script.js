455a791b190096c682fcf4b17f221912a07223d8
sayudha <mazaya.studio@outlook.com>
2024-01-04 15:44:26 +0700

    JS - Favorites DOM Elements: Refactor updateDOM function and add createDOMNodes function

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKEY}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-function updateDOM() {
-	resultsArray.forEach((result) => {
+function createDOMNodes(page) {
+	const currentArray = page === "results" ? resultsArray : Object.values(favorites);
+	console.log('Current Array', page, currentArray);
+	currentArray.forEach((result) => {
 		// Card Container
 		const card = document.createElement("div");
 		card.classList.add("card");
@@ -62,13 +64,20 @@ function updateDOM() {
 	});
 }
 
+function updateDOM(page) {
+	// Get Favorites from localStorage
+	if (localStorage.getItem("nasaFavorites")) {
+		favorites = JSON.parse(localStorage.getItem("nasaFavorites"));
+	}
+	createDOMNodes(page);
+}
+
 // get 10 images from NASA API
 async function getNasaPictures() {
 	try {
 		const response = await fetch(apiURL);
 		resultsArray = await response.json();
-		console.log(resultsArray);
-		updateDOM();
+		updateDOM("favorites");
 	} catch (error) {
 		// Catch Error Here
 	}
